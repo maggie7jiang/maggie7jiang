3e1598b7a63e200ece29e0d19d003923475373e6
Maggie Jiang <68176129+maggie7jiang@users.noreply.github.com>
2021-10-26 17:33:37 -0500

    Create L83-Remove-Duplicates-from-Sorted-List.cpp

---
+++ folder/week2/L83-Remove-Duplicates-from-Sorted-List.cpp
@@ -0,0 +1,31 @@
+//83. Remove Duplicates from Sorted List
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode *current = head;
+        while (current != nullptr && current->next != nullptr)
+        {
+            if (current->val == current->next->val)
+            {
+                current->next = current->next->next;
+            }
+            else
+            {
+                current = current->next;
+            }
+        }
+        return head;
+    }
+};
+
+//do we need to release the memory here?
